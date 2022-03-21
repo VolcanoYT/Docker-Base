52f640bb09289728d45c0a6c1b3bdb3d629f1c80
Akbar Yahya <11765082+akbaryahya@users.noreply.github.com>
2022-03-21 11:28:52 +0800

    update php v8

--- php-v8-alpine/config/entrypoint.sh
+++ php-v8-alpine/config/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf /var/www/html/composer.lock
-composer update --no-interaction --no-cache --optimize-autoloader
+composer update --ignore-platform-reqs --no-interaction --no-cache --optimize-autoloader
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
